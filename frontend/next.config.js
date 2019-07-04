b5f998a6de34eb00e8d05baa43e941df04ce179a
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-04 15:07:27 +0900

    Add TypeScript type checker in webpack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,14 @@
+const path = require('path')
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    if (options.isServer) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({ tsconfig: path.resolve(__dirname, '../tsconfig.json') })
+      )
+    }
+    return config
+  }
+})
