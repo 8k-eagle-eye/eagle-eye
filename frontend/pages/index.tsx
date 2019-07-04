a7a2dc0fc2b696fb534ee433637803e30e602fa0
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-04 13:45:57 +0900

    Replace CSS using styled-components

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react'
 import Link from 'next/link'
+import styled from 'styled-components'
 import Head from '../components/head'
 import Nav from '../components/nav'
+
+const Hero = styled.div`
+  width: 100%;
+  color: #333;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  width: 100%;
+  padding-top: 80px;
+  line-height: 1.15;
+  font-size: 48px;
+  text-align: center;
+`
+
+const Description = styled.p`
+  text-align: center;
+`
+
+const Row = styled.div`
+  max-width: 880px;
+  margin: 80px auto 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+
+const Card = styled.a`
+  padding: 18px 18px 24px;
+  width: 220px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+
+  &:hover {
+    border-color: #067df7;
+  }
+
+  & h3 {
+    margin: 0;
+    color: #067df7;
+    font-size: 18px;
+  }
+
+  & p {
+    margin: 0;
+    padding: 12px 0 0;
+    font-size: 13px;
+    color: #333;
+  }
+`
 
 const Home = () => (
   <div>
@@ -38,53 +91,6 @@ const Home = () => (
         </Link>
       </div>
     </div>
-
-    {/* <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style> */}
   </div>
 )
 
