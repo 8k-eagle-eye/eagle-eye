fc3c5dc869f22c059fb9d3b9796a852fd9ae1a73
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-05 23:18:53 +0900

    Display server URL

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import tachijs, { controller, httpGet } from 'tachijs'
 
+const port = 8000
+
 @controller('/')
 class HomeController {
   // Define when this method should be used.
@@ -18,4 +20,6 @@ const app = tachijs({
 })
 
 // `app` is just an express application instance
-app.listen(8000)
+app.listen(port, () => {
+  console.log(`[server]  ready on http://localhost:${port}`)
+})
