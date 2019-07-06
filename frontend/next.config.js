a38e056c2b2a6b565781dc0ffc7f810fa38f484f
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-06 21:23:14 +0900

    Add eslint-loader

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,24 @@ const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = withTypescript({
-  webpack(config, options) {
-    if (options.isServer) {
+  webpack(config, { dev, isServer }) {
+    if (dev && isServer) {
+      config.module.rules.push({
+        enforce: 'pre',
+        exclude: /(node_modules)/,
+        loader: 'eslint-loader',
+        test: /\.(js|jsx|ts|tsx)$/
+      })
+    }
+
+    if (isServer) {
       config.plugins.push(
         new ForkTsCheckerWebpackPlugin({
           tsconfig: path.join(__dirname, './tsconfig.json')
         })
       )
     }
+
     return config
   }
 })
