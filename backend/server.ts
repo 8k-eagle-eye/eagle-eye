121334b0ab8fad632f633b19048f919251b7c417
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-10 23:33:02 +0900

    Update nodemon configuration

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import app from 'backend/app'
 
 const { NODE_ENV, PORT } = process.env
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT || 8000, () => {
   if (NODE_ENV === 'development') {
     // eslint-disable-next-line no-console
     console.log(`[server]  ready on http://localhost:${server.address().port}`)
