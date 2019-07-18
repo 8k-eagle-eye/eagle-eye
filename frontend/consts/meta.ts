4bcaa90b378a2b69c768334e80c7dc623d0ae615
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-19 02:40:40 +0900

    Add header component

---
+++ frontend/consts/meta.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = '8K Eagle Eye'
+export const APP_VERSION = 'Alpha'
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import Head from 'components/head'
+import Header from 'components/public/header'
 import Viewer from 'components/viewer'
 import { ResetStyle } from 'assets/styles/globalStyle'
+import { SITE_TITLE, APP_VERSION } from 'consts/meta'
 
 const ViewerFrame = styled.div`
   width: 600px;
@@ -11,8 +13,9 @@ const ViewerFrame = styled.div`
 
 const Home = () => (
   <div>
-    <Head title="Home" />
+    <Head title={SITE_TITLE} />
     <ResetStyle />
+    <Header title={SITE_TITLE} version={APP_VERSION} />
 
     <ViewerFrame>
       <Viewer aspect={16 / 9} />
