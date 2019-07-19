a94ee5cd2a3af9e852ea201a725797f087dcacd6
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-07-19 12:17:47 +0900

    Add Features

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import Head from 'components/head'
 import Header from 'components/public/header'
 import Hero from 'components/public/hero'
 import Viewer from 'components/viewer'
+import Feature, { FeatureProps } from 'components/public/feature'
 import { ResetStyle } from 'assets/styles/globalStyle'
 import { SITE_TITLE, APP_VERSION } from 'consts/meta'
+
+interface FeaturesProps {
+  features: FeatureProps[]
+}
 
 const dummyText = `
 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -14,6 +19,14 @@ Nostrum harum laudantium dicta laboriosam iste molestias totam
 aperiam, iusto consectetur obcaecati, facilis velit, voluptatum
 nisi sed. Vero molestias minus sequi voluptatem!
 `
+
+const featureContents = [...Array(3)].map(
+  (_, idx): FeatureProps => ({
+    heading: `heading ${++idx}`,
+    description: dummyText,
+    icon: { src: '/static/images/github.svg' }
+  })
+)
 
 const ViewerSection = styled.section`
   text-align: center;
@@ -42,6 +55,53 @@ const ViewerFrame = styled.div`
   max-width: 640px;
 `
 
+const FeaturesSection = styled.section`
+  position: relative;
+  overflow-x: hidden;
+  padding-top: 6vw;
+  background-color: ${({ theme }) => theme.color.primaryLight};
+  background-clip: content-box;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    top: 1px;
+    z-index: -1;
+    display: block;
+    height: 0;
+    width: 0;
+    border-style: solid;
+  }
+
+  &::before {
+    right: 0;
+    border-color: transparent transparent ${({ theme }) => theme.color.primaryDark} transparent;
+    border-width: 0 0 6vw 100vw;
+  }
+
+  &::after {
+    left: 0;
+    border-color: transparent transparent transparent ${({ theme }) => theme.color.primaryLight};
+    border-width: 6vw 0 0 100vw;
+  }
+`
+
+const Features = (props: FeaturesProps) => (
+  <FeaturesSection>
+    <Container>
+      <h2 className="font-weight-bold text-center mt-5 mb-md-5">Features</h2>
+      <Row>
+        {props.features.map(({ heading, description, icon }) => (
+          <Col key={heading} md className="mt-5 mt-md-0">
+            <Feature heading={heading} description={description} icon={icon} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  </FeaturesSection>
+)
+
 const Home = () => (
   <div>
     <Head title={SITE_TITLE} />
@@ -66,6 +126,8 @@ const Home = () => (
         <p>{dummyText}</p>
       </Container>
     </ViewerSection>
+
+    <Features features={featureContents} />
   </div>
 )
 
