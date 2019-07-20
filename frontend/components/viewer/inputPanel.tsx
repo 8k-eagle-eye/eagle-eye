6532ba937a069eccead1c0a0f90f8b076c0bbdb4
m-mitsuhide <genbu2011@live.jp>
2019-07-20 17:26:48 +0900

    スマホ対応

--- frontend/components/viewer/inputPanel.tsx
+++ frontend/components/viewer/inputPanel.tsx
@@ -1,6 +1,7 @@
 import React, { Component, createRef, Dispatch } from 'react'
 import styled from 'styled-components'
 import { ZOOM_STEP, MAX_SCALE } from 'consts/viewer'
+import calcDistance from 'libs/viewer/calcDistance.ts'
 
 interface InputPanelProps {
   baseSize: { width: number; height: number }
@@ -23,12 +24,25 @@ export default class InputPanel extends Component<InputPanelProps> {
   private prevPanPoint = { x: 0, y: 0 }
   private zoomCenter = { x: 0, y: 0 }
   private panning = false
+  private zooming = false
+  private baseDistance = 0
   private inputPanelRef = createRef<HTMLDivElement>()
 
   private onMouseDown = (e: React.MouseEvent) => {
     e.preventDefault()
     this.panning = true
     this.startPan(e)
+  }
+
+  private onTouchStart = (e: TouchEvent) => {
+    e.preventDefault()
+
+    if (e.touches.length === 1) {
+      this.startPan(e.touches[0])
+    } else if (e.touches.length === 2) {
+      this.zooming = true
+      this.baseDistance = calcDistance(e.touches) / this.props.scale
+    }
   }
 
   private onMouseMove = (e: MouseEvent) => {
@@ -39,8 +53,25 @@ export default class InputPanel extends Component<InputPanelProps> {
     this.movePan(e)
   }
 
+  private onTouchMove = ({ touches }: TouchEvent) => {
+    if (touches.length === 1 && !this.zooming) {
+      this.movePan(touches[0])
+    } else if (touches.length === 2) {
+      this.onZoom(
+        (touches[0].pageX + touches[1].pageY) / 2,
+        (touches[0].pageY + touches[1].pageY) / 2,
+        calcDistance(touches) / this.baseDistance - this.props.scale
+      )
+    }
+  }
+
   private onMouseUp = () => {
     this.panning = false
+  }
+
+  private onTouchEnd = ({ touches }: TouchEvent) => {
+    // ズーム後に指1本でパンに移行してガタつくのを防ぐ
+    if (touches.length === 0) this.zooming = false
   }
 
   private onWheel = (e: WheelEvent) => {
@@ -48,11 +79,11 @@ export default class InputPanel extends Component<InputPanelProps> {
     this.onZoom(e.clientX, e.clientY, e.deltaY < 0 ? ZOOM_STEP : -ZOOM_STEP)
   }
 
-  private startPan(e: React.MouseEvent) {
+  private startPan(e: React.MouseEvent | Touch) {
     this.prevPanPoint = { x: e.pageX, y: e.pageY }
   }
 
-  private movePan(e: MouseEvent) {
+  private movePan(e: MouseEvent | Touch) {
     const { prevPanPoint } = this
     const currentPanPoint = { x: e.pageX, y: e.pageY }
 
@@ -96,13 +127,19 @@ export default class InputPanel extends Component<InputPanelProps> {
   public componentDidMount() {
     document.addEventListener('mousemove', this.onMouseMove, false)
     document.addEventListener('mouseup', this.onMouseUp, false)
+    document.addEventListener('touchmove', this.onTouchMove, false)
+    document.addEventListener('touchend', this.onTouchEnd, false)
     this.inputPanelRef.current!.addEventListener('wheel', this.onWheel, false)
+    this.inputPanelRef.current!.addEventListener('touchstart', this.onTouchStart, false)
   }
 
   public componentWillUnmount() {
     document.removeEventListener('mousemove', this.onMouseMove, false)
     document.removeEventListener('mouseup', this.onMouseUp, false)
+    document.removeEventListener('touchmove', this.onTouchMove, false)
+    document.removeEventListener('touchend', this.onTouchEnd, false)
     this.inputPanelRef.current!.removeEventListener('wheel', this.onWheel, false)
+    this.inputPanelRef.current!.removeEventListener('touchstart', this.onTouchStart, false)
   }
 
   public render() {
