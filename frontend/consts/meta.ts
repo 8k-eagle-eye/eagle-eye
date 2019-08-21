5a2c366ff5a2ec1a060051ee236342467f9eadd7
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-08-21 21:02:34 +0900

    refactor: update metadata in Head component

--- frontend/consts/meta.ts
+++ frontend/consts/meta.ts
@@ -1,2 +1,6 @@
-export const SITE_TITLE = '8K Eagle Eye'
+import pkg from '~/package.json'
+
 export const COPYRIGHT = '© 2019 8K Eagle Eye'
+export const SITE_DESCRIPTION = pkg.description
+export const SITE_TITLE = '8K Eagle Eye'
+export const SITE_URL = 'https://8kee.netlify.com/'
