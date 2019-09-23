e7507023d0740e46a85cb82af44ea733d1c61896
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-09-23 16:38:29 +0900

    chore: refactor moduleNameMapper option in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,10 @@
-const path = require('path')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig')
-
-const baseUrl = path.resolve(process.cwd(), compilerOptions.baseUrl)
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    ...Object.entries(compilerOptions.paths).reduce((obj, cv) => {
-      const reg = new RegExp('\\/?\\*$')
-      const key = cv[0].replace(reg, '')
-      const value = cv[1][0].replace(reg, '')
-
-      if (/^.*(?<!\/\*)$/.test(cv[0])) {
-        obj[`^${key}$`] = `${path.resolve(baseUrl, value)}`
-      }
-
-      if (/\*$/.test(cv[0])) {
-        obj[`^${key}/(.*)`] = `${path.resolve(baseUrl, value)}/$1`
-      }
-
-      return obj
-    }, {})
-  }
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  })
 }
